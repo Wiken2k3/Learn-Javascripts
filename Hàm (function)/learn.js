68d89a45a4c67ca4cd0a451f409f909e429571f5
Wiken2k3 <wiken2k3@gmail.com>
2025-08-09 16:08:56 +0700

    Tạo hàm

---
+++ Hàm (function)/learn.js
@@ -0,0 +1,26 @@
+/**
+Hàm :
+-Là một khối mã
+- Làm một việc cụ thể
+
+Loại hàm:
+- Built-in
+- Tự định nghĩa
+
+Tính chất :
+- Không thực thi khi định nghĩa
+- Sẽ thực thi khi được gọi
+- Có thể nhận tham số
+- Có thể trả về 1 giá trị
+
+Tạo hàm đầu tiên :
+*/
+// a-z A-Z 0-9 _ $ 
+
+
+function showDialog(){
+    alert('Hello xin chào các bạn');
+}
+alert();
+
+showDialog();
---
+++ Hàm (function)/thuchanh2.js
@@ -0,0 +1,18 @@
+/**Vượt qua thử thách này bằng cách tạo một hàm tên là sum.
+
+Chỉ cần tạo hàm, không cần viết gì trong phần thân của hàm. */
+
+function sum(message){  //Tạo hàm tên là sum
+   console.log(message);
+}
+sum("Hello mọi người");
+
+
+/**Vượt qua thử thách này bằng cách tạo một hàm tên là triple.
+
+Chỉ cần tạo hàm, không cần viết gì trong phần thân của hàm. */
+
+function triple(message){
+    console.log(message);
+}
+triple("xin chào các bạn");

b9f850216c386eb8f351a2f78383f5164764ccc3
Wiken2k3 <wiken2k3@gmail.com>
2025-08-10 11:38:37 +0700

    return tt

--- Hàm (function)/Return trong hàm/thuchanh.js
+++ Hàm (function)/Return trong hàm/thuchanh.js
@@ -8,3 +8,41 @@ function sum(a,b){
 var result = sum (1,4);
 
 console.log(result)
+
+
+/**Sử dụng hàm sum đã cho, hãy tạo biến result để 
+lưu kết quả của phép cộng 2 số là 20 và 10.
+
+Sau đó, bạn có thể console.log(result) và 
+mở tab Console trong Dev Tool để xem giá trị của 
+result một cách trực quan. */
+
+function sum(a,b){
+    return a +b ;
+}
+
+var result = sum (20,10);
+
+console.log(result)
+
+
+/**Hoàn thành thử thách này bằng cách tạo hàm triple, 
+hàm có 1 tham số x và trả về giá trị gấp 3 lần x. 
+Ví dụ, x = 3 thì hàm triple sẽ trả về 9. */
+function triple(x){
+    return x *3 ;
+}
+
+var result = triple(3);
+console.log(result)
+
+
+/**Sử dụng hàm triple đã cho, 
+hãy tạo biến result để lưu kết quả của 10 * 3. */
+function triple(x){
+    return x *3 ;
+}
+
+var result = triple(10);
+console.log(result)
+

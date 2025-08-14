d87e2dd122cfe10e3fe6a75ff564a8540ed1e465
Wiken2k3 <wiken2k3@gmail.com>
2025-08-14 11:57:35 +0700

    constructor

---
+++ Object/Constructor/learn.js
@@ -0,0 +1,80 @@
+// Object Constructor
+
+function User(firstName, lastName, avatar){
+    this.firstName  = firstName;
+    this.lastName  = lastName;
+    this.avatar  = avatar;
+
+    
+
+}
+var author = new User('Tuan','Tran','Avatar');
+var user = new User('Duc','Tran','Avatar');
+
+
+console.log(author.constructor);  //để trả về mô tả của cấu trúc      
+console.log(user);
+
+
+
+
+//thêm thuộc tính cho từng role riêng
+function User(firstName, lastName, avatar){
+    this.firstName  = firstName;
+    this.lastName  = lastName;
+    this.avatar  = avatar;
+
+}
+var author = new User('Tuan','Tran','Avatar');
+var user = new User('Duc','Tran','Avatar');
+
+author.title = "Xin chào các bạn";
+user.commet = "Thật là hay ";
+
+console.log(author);       
+console.log(user);
+
+
+
+//thêm phương thức biến function getName để nối thành fullname
+
+function User(firstName, lastName, avatar) { 
+    this.firstName  = firstName;
+    this.lastName  = lastName;  
+    this.avatar  = avatar;
+
+    this.getName = function() {
+        return `${this.firstName} ${this.lastName}`;    //this khi gọi từ đối tượng nào thì nó sẽ là đối tượng đó
+    }
+}
+
+var author = new User('Tuan', 'Tran', 'Avatar');
+var user = new User('Duc', 'Tran', 'Avatar');
+
+author.title = "Xin chào các bạn";
+user.comment = "Thật là hay";
+
+console.log(author.getName());  // Tuan Tran
+console.log(user.getName());    // Duc Tran
+
+//Chuyển thành Express function 
+
+var User= function(firstName, lastName, avatar) { 
+    this.firstName  = firstName;
+    this.lastName  = lastName;  
+    this.avatar  = avatar;
+
+    this.getName = function() {
+        return `${this.firstName} ${this.lastName}`;    //this khi gọi từ đối tượng nào thì nó sẽ là đối tượng đó
+    }
+}
+
+var author = new User('Tuan', 'Tran', 'Avatar');
+var user = new User('Duc', 'Tran', 'Avatar');
+
+author.title = "Xin chào các bạn";
+user.comment = "Thật là hay";
+
+console.log(author);  // Tuan Tran
+console.log(user);    // Duc Tran
+
---
+++ Object/thuchanh.js
@@ -0,0 +1,12 @@
+/**Tạo biến student và gán cho nó 1 object, object 
+này có 3 keys để thể hiện tên, tuổi và địa chỉ của sinh viên. 
+Các keys có tên là name, age và address.
+
+Kiểu dữ liệu của các keys các bạn tự tư duy sử dụng cho đúng đắn nhé. */
+
+var student = {     //Tạo object student đúng theo yêu cầu
+    name: 'Tuan Tran',
+    age: 22,
+    address: 'Ho Chi Minh'
+}
+console.log(student)

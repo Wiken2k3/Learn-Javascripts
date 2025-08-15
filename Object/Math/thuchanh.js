2ed8ab4efcdb2363190f155a957e3afd136a5543
Wiken2k3 <wiken2k3@gmail.com>
2025-08-15 08:52:16 +0700

    bai tap math

---
+++ Object/Math/thuchanh.js
@@ -0,0 +1,12 @@
+/**Lấy ngẫu nhiên phần tử của mảng
+Hãy tạo hàm getRandomItem, hàm này nhận 1 tham số là mảng 
+và sẽ trả về ngẫu nhiên 1 phần tử của mảng.
+ */
+
+function getRandomItem(array) {
+    var random = Math.floor(Math.random() * array.length);
+    return array[random];
+}
+
+
+//Tạo hàm getRandomItem theo yêu cầu đề bài

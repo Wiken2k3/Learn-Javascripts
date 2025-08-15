640b4b55d8d7cb7bf00adb101e2a108c3d1493e8
Wiken2k3 <wiken2k3@gmail.com>
2025-08-15 10:04:28 +0700

    if else

---
+++ If else & Toán tử 3 ngôi/learn.js
@@ -0,0 +1,11 @@
+var date = 5;
+
+if (date === 2){    //luôn luôn chỉ lọt vào 1 nhánh đúng =>chỉ trả ra nhánh đúng đó 
+    console.log('Hôm nay là thứ 2');
+}else if (date === 3){
+    console.log('Hôm nay là thứ 3');
+}else if (date === 4){
+    console.log('Hôm nay là thứ 4');
+} else{
+    console.log('Không biết');
+}

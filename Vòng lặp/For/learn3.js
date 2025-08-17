3ab55b4122e8f5f4b86a18d348b37ccce3ec13d1
Wiken2k3 <wiken2k3@gmail.com>
2025-08-17 20:22:21 +0700

    loop 3

---
+++ Vòng lặp/For/learn3.js
@@ -0,0 +1,13 @@
+// For loop part 3:
+var myArray =[
+    'HTML',
+    'CSS',
+    'Java',
+    'React'
+]
+
+var arrayLength = myArray.length;
+
+for (var i = 0; i < arrayLength; i++){
+    console.log(myArray[i]);
+}

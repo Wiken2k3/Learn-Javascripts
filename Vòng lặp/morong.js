876b441aa12c8b7a36c8c62a2da596c42275c6e4
Wiken2k3 <wiken2k3@gmail.com>
2025-08-18 17:26:42 +0700

    loop mo rong

---
+++ Vòng lặp/morong.js
@@ -0,0 +1,21 @@
+// Loop mở rộng
+
+for (var i = 100 ; i > 0 ; i--){
+    console.log(i);
+}
+
+
+//in ra giá trị 0,5,15
+
+for (var i = 0 ; i <= 100 ; i +=5){
+    console.log(i);
+}
+
+
+//ngược lại in từ 100 về 0 (cách 5)
+
+//in ra giá trị 0,5,15
+
+for (var i = 100 ; i >= 0 ; i -=5){
+    console.log(i);
+}

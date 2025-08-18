f7fe969c400e9aaab3e2607632e0dd3f709eb211
Wiken2k3 <wiken2k3@gmail.com>
2025-08-18 16:36:25 +0700

    break and continue

--- Vòng lặp/Break & Continue/learn.js
+++ Vòng lặp/Break & Continue/learn.js
@@ -1,1 +1,19 @@
 //break (thoát khỏi vòng lặp) và Continue (bỏ qua một vài đoạn) của vòng lặp
+
+for (var i = 0; i < 10; i++ )    {
+    console.log(i);
+
+    if (i >= 5 ){   
+        break;          //nếu i >= 5 thì từ khoá break sẽ được thực thi
+    }   
+}
+
+
+for (var i = 0; i < 10; i++ )    {
+    if (i%2 !==0){
+        continue;       //TH điều kiện này thoả mãn thì sẽ tiếp tục vòng lặp của điều kiện và bỏ các vòng đang chạy
+    }                   //nên những lần là số lẻ của điều kiện bị bỏ qua và chỉ lấy số chẵn
+    console.log(i);
+
+}
+
---
+++ Vòng lặp/Nested loop/learn.js
@@ -0,0 +1,9 @@
+// Vòng lặp lồng nhauu:
+var myArray = [
+    [1, 2],
+    [3, 4],
+    [5, 6]
+];
+for (var i = 0; i < myArray.length ; i++){
+    console.log(myArray[i]);
+}

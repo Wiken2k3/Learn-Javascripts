4bca1b8081989bc49ee3b6bebec0621cd208626b
Wiken2k3 <wiken2k3@gmail.com>
2025-08-18 17:13:56 +0700

    nested loop

--- Vòng lặp/Nested loop/learn.js
+++ Vòng lặp/Nested loop/learn.js
@@ -4,6 +4,10 @@ var myArray = [
     [3, 4],
     [5, 6]
 ];
-for (var i = 0; i < myArray.length ; i++){
-    console.log(myArray[i]);
+for (var i = 0; i < myArray.length ; i++){  
+    for (var j = 0 ; j < myArray[i].length ; j++ ){
+        console.log(myArray[i][j]);
+    }
 }
+
+ 

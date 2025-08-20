5323b009e4f48f5a1cddec816108eb3e8423a9d6
Wiken2k3 <wiken2k3@gmail.com>
2025-08-20 23:02:07 +0700

    every array

--- Mảng/Array Method Part 2/learn.js
+++ Mảng/Array Method Part 2/learn.js
@@ -36,7 +36,7 @@ var courses = [
     }
 ];
 
-// forEach:
+// forEach: duyệt qua phần tử của mảng
 courses.forEach(function(course){
     console.log(course);
 })
@@ -50,3 +50,9 @@ courses.forEach(function(course){
 
 
 
+//  every(): mỗi khi lặp vào từng phần tử của mảng và mỗi lần lặp thì nó sẽ gọi thì sẽ gọi ngược vào hàm đã truyền vào function
+var isFree = courses.every(function(course, index){     // và nó truyền vào tham số thứ nhất (course : phần tử mảng chúng ta)
+    return course.coin === 0;       //bắt đầu ta so sánh nếu biểu thức điều kiện trả về là đúng thì nó mới kiểm tra phần tử tiếp theo và tương tự ...
+})              //nhưng chỉ 1 thằng sai thì sẽ trả về là false
+
+console.log(isFree);  //nếu các điều kiện đúng hết thì sẽ trả về là true

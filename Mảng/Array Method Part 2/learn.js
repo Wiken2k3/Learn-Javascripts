c8f084af1125abceba2b57c40d375ae82ad259c2
Wiken2k3 <wiken2k3@gmail.com>
2025-08-20 23:09:46 +0700

    find array

--- Mảng/Array Method Part 2/learn.js
+++ Mảng/Array Method Part 2/learn.js
@@ -1,8 +1,8 @@
 /**Array methods:
     forEach()   //duyệt qua phần tử của mảng
-    every()     
-    some()
-    find()
+    every()     //Dùng để kiểm tra tất cả phần tử thoả mãn 1 điều kiện nào đó
+    some()      //CHỉ cần 1 thoả mãn điều kiện đó rồi sẽ ngừng lại vòng lặp
+    find()      //tìm kiếm 
     filter()
     map()
     reduce()
@@ -50,9 +50,27 @@ courses.forEach(function(course){
 
 
 
-//  every(): mỗi khi lặp vào từng phần tử của mảng và mỗi lần lặp thì nó sẽ gọi thì sẽ gọi ngược vào hàm đã truyền vào function
+//  every():   mỗi khi lặp vào từng phần tử của mảng và mỗi lần lặp thì nó sẽ gọi thì sẽ gọi ngược vào hàm đã truyền vào function
 var isFree = courses.every(function(course, index){     // và nó truyền vào tham số thứ nhất (course : phần tử mảng chúng ta)
     return course.coin === 0;       //bắt đầu ta so sánh nếu biểu thức điều kiện trả về là đúng thì nó mới kiểm tra phần tử tiếp theo và tương tự ...
 })              //nhưng chỉ 1 thằng sai thì sẽ trả về là false
 
 console.log(isFree);  //nếu các điều kiện đúng hết thì sẽ trả về là true
+
+
+
+//some()  ngược lại với every
+var isFree = courses.some(function(course, index){     
+    return course.coin === 0;       
+})              
+
+console.log(isFree);
+
+
+
+//some()   
+var course = courses.find(function(course, index){     
+    return course.name === 'HTML';    //nếu hàm nào return là true thì sẽ gán ngược vào vòng lặp đó rồi sẽ kết thúc
+})              
+
+console.log(course);
